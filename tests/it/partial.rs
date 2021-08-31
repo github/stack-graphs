e1d9edd234e63e1e918c168706de289d60f9c82d
Douglas Creager <dcreager@dcreager.net>
2021-08-30 20:57:56 -0400

    Add function for unifying partial symbol stacks
    This is very much like the function that we recently addded for unifying
    partial scope stacks.  There is some extra complexity from having to
    deal with attached scope stacks.

--- tests/it/partial.rs
+++ tests/it/partial.rs
@@ -5,18 +5,269 @@
 // Please see the LICENSE-APACHE or LICENSE-MIT files in this distribution for license details.
 // ------------------------------------------------------------------------------------------------
 
+use controlled_option::ControlledOption;
 use stack_graphs::graph::NodeID;
 use stack_graphs::graph::StackGraph;
 use stack_graphs::partial::PartialPaths;
 use stack_graphs::partial::PartialScopeStack;
 use stack_graphs::partial::PartialScopeStackBindings;
+use stack_graphs::partial::PartialScopedSymbol;
+use stack_graphs::partial::PartialSymbolStack;
+use stack_graphs::partial::PartialSymbolStackBindings;
 use stack_graphs::partial::ScopeStackVariable;
+use stack_graphs::partial::SymbolStackVariable;
 use stack_graphs::paths::PathResolutionError;
+
+type NiceSymbolStack<'a> = (&'a [NiceScopedSymbol<'a>], Option<SymbolStackVariable>);
+type NiceScopedSymbol<'a> = (&'a str, Option<NiceScopeStack<'a>>);
+type NiceScopeStack<'a> = (&'a [u32], Option<ScopeStackVariable>);
+
+fn create_symbol_stack(
+    graph: &mut StackGraph,
+    partials: &mut PartialPaths,
+    contents: NiceSymbolStack,
+) -> PartialSymbolStack {
+    let mut stack = if let Some(var) = contents.1 {
+        PartialSymbolStack::from_variable(var)
+    } else {
+        PartialSymbolStack::empty()
+    };
+    for scoped_symbol in contents.0 {
+        let symbol = graph.add_symbol(scoped_symbol.0);
+        let scopes = scoped_symbol
+            .1
+            .map(|scopes| create_scope_stack(graph, partials, scopes));
+        let scoped_symbol = PartialScopedSymbol {
+            symbol,
+            scopes: ControlledOption::from_option(scopes),
+        };
+        stack.push_back(partials, scoped_symbol);
+    }
+    stack
+}
+
+#[test]
+fn can_unify_partial_symbol_stacks() -> Result<(), PathResolutionError> {
+    fn verify(
+        lhs: NiceSymbolStack,
+        rhs: NiceSymbolStack,
+        expected_unification: &str,
+        expected_symbol_bindings: &str,
+        expected_scope_bindings: &str,
+    ) -> Result<(), PathResolutionError> {
+        let mut graph = StackGraph::new();
+        let mut partials = PartialPaths::new();
+        let lhs = create_symbol_stack(&mut graph, &mut partials, lhs);
+        let rhs = create_symbol_stack(&mut graph, &mut partials, rhs);
+        let mut symbol_bindings = PartialSymbolStackBindings::new();
+        let mut scope_bindings = PartialScopeStackBindings::new();
+        let unified = lhs.unify(
+            &mut partials,
+            rhs,
+            &mut symbol_bindings,
+            &mut scope_bindings,
+        )?;
+        let unified = unified.display(&graph, &mut partials).to_string();
+        assert_eq!(unified, expected_unification);
+        let symbol_bindings = symbol_bindings.display(&graph, &mut partials).to_string();
+        assert_eq!(symbol_bindings, expected_symbol_bindings);
+        let scope_bindings = scope_bindings.display(&graph, &mut partials).to_string();
+        assert_eq!(scope_bindings, expected_scope_bindings);
+        Ok(())
+    }
+
+    fn verify_not(lhs: NiceSymbolStack, rhs: NiceSymbolStack) -> Result<(), PathResolutionError> {
+        let mut graph = StackGraph::new();
+        let mut partials = PartialPaths::new();
+        let lhs = create_symbol_stack(&mut graph, &mut partials, lhs);
+        let rhs = create_symbol_stack(&mut graph, &mut partials, rhs);
+        let mut symbol_bindings = PartialSymbolStackBindings::new();
+        let mut scope_bindings = PartialScopeStackBindings::new();
+        assert!(lhs
+            .unify(
+                &mut partials,
+                rhs,
+                &mut symbol_bindings,
+                &mut scope_bindings
+            )
+            .is_err());
+        Ok(())
+    }
+
+    let var1 = Some(SymbolStackVariable::new(1).unwrap());
+    let var2 = Some(SymbolStackVariable::new(2).unwrap());
+    let a = ("a", None);
+
+    verify((&[], None), (&[], None), "", "{}", "{}")?;
+    verify((&[], var1), (&[], None), "%1", "{%1 => <>}", "{}")?;
+    verify((&[], None), (&[], var2), "%2", "{%2 => <>}", "{}")?;
+    verify((&[], var1), (&[], var2), "%1", "{%2 => <%1>}", "{}")?;
+
+    verify_not(
+        (&[], None), //
+        (&[a], None),
+    )?;
+    verify(
+        (&[], var1), //
+        (&[a], None),
+        "a",
+        "{%1 => <a>}",
+        "{}",
+    )?;
+    verify_not(
+        (&[], None), //
+        (&[a], var2),
+    )?;
+    verify(
+        (&[], var1), //
+        (&[a], var2),
+        "a,%2",
+        "{%1 => <a,%2>}",
+        "{}",
+    )?;
+
+    verify_not(
+        (&[a], None), //
+        (&[], None),
+    )?;
+    verify_not(
+        (&[a], var1), //
+        (&[], None),
+    )?;
+    verify(
+        (&[a], None), //
+        (&[], var2),
+        "a",
+        "{%2 => <a>}",
+        "{}",
+    )?;
+    verify(
+        (&[a], var1), //
+        (&[], var2),
+        "a,%1",
+        "{%2 => <a,%1>}",
+        "{}",
+    )?;
+
+    verify(
+        (&[a], None), //
+        (&[a], None),
+        "a",
+        "{}",
+        "{}",
+    )?;
+    verify(
+        (&[a], var1), //
+        (&[a], None),
+        "a,%1",
+        "{%1 => <>}",
+        "{}",
+    )?;
+    verify(
+        (&[a], None), //
+        (&[a], var2),
+        "a,%2",
+        "{%2 => <>}",
+        "{}",
+    )?;
+    verify(
+        (&[a], var1), //
+        (&[a], var2),
+        "a,%1",
+        "{%2 => <%1>}",
+        "{}",
+    )?;
+
+    let dot = (".", None);
+    let b = ("b", None);
+
+    verify(
+        (&[a, dot, b], None),
+        (&[a, dot, b], None),
+        "a.b",
+        "{}",
+        "{}",
+    )?;
+    verify(
+        (&[a, dot, b], var1),
+        (&[a, dot, b], None),
+        "a.b,%1",
+        "{%1 => <>}",
+        "{}",
+    )?;
+    verify(
+        (&[a, dot, b], None),
+        (&[a, dot, b], var2),
+        "a.b,%2",
+        "{%2 => <>}",
+        "{}",
+    )?;
+    verify(
+        (&[a, dot, b], var1),
+        (&[a, dot, b], var2),
+        "a.b,%1",
+        "{%2 => <%1>}",
+        "{}",
+    )?;
+
+    verify_not(
+        (&[a], None), //
+        (&[a, dot, b], None),
+    )?;
+    verify_not(
+        (&[a], None), //
+        (&[a, dot, b], var2),
+    )?;
+    verify(
+        (&[a], var1), //
+        (&[a, dot, b], None),
+        "a.b",
+        "{%1 => <.b>}",
+        "{}",
+    )?;
+    verify(
+        (&[a], var1), //
+        (&[a, dot, b], var2),
+        "a.b,%2",
+        "{%1 => <.b,%2>}",
+        "{}",
+    )?;
+
+    let empty_scopes: NiceScopeStack = (&[], None);
+    let a_empty = ("a", Some(empty_scopes));
+
+    let scope_var1 = Some(ScopeStackVariable::new(1).unwrap());
+    let scopes_var1: NiceScopeStack = (&[], scope_var1);
+    let a_var1 = ("a", Some(scopes_var1));
+
+    verify_not((&[a], None), (&[a_empty], None))?;
+    verify_not((&[a_empty], None), (&[a], None))?;
+
+    verify_not((&[a], None), (&[a_var1], None))?;
+    verify_not((&[a_var1], None), (&[a], None))?;
+
+    verify(
+        (&[a_empty], None),
+        (&[a_var1], None),
+        "a/($1)",
+        "{}",
+        "{$1 => ()}",
+    )?;
+    verify(
+        (&[a_var1], None),
+        (&[a_empty], None),
+        "a/($1)",
+        "{}",
+        "{$1 => ()}",
+    )?;
+
+    Ok(())
+}
 
 fn create_scope_stack(
     graph: &mut StackGraph,
     partials: &mut PartialPaths,
-    contents: (&[u32], Option<ScopeStackVariable>),
+    contents: NiceScopeStack,
 ) -> PartialScopeStack {
     let file = graph.get_or_create_file("file");
     let mut stack = if let Some(var) = contents.1 {
@@ -38,8 +289,8 @@ fn create_scope_stack(
 #[test]
 fn can_unify_partial_scope_stacks() -> Result<(), PathResolutionError> {
     fn verify(
-        lhs: (&[u32], Option<ScopeStackVariable>),
-        rhs: (&[u32], Option<ScopeStackVariable>),
+        lhs: NiceScopeStack,
+        rhs: NiceScopeStack,
         expected_unification: &str,
         expected_bindings: &str,
     ) -> Result<(), PathResolutionError> {
@@ -56,10 +307,7 @@ fn can_unify_partial_scope_stacks() -> Result<(), PathResolutionError> {
         Ok(())
     }
 
-    fn verify_not(
-        lhs: (&[u32], Option<ScopeStackVariable>),
-        rhs: (&[u32], Option<ScopeStackVariable>),
-    ) -> Result<(), PathResolutionError> {
+    fn verify_not(lhs: NiceScopeStack, rhs: NiceScopeStack) -> Result<(), PathResolutionError> {
         let mut graph = StackGraph::new();
         let mut partials = PartialPaths::new();
         let lhs = create_scope_stack(&mut graph, &mut partials, lhs);
