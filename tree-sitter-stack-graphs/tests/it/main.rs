343aea026b20de0e45e3147b59dfed976ed65402
Douglas Creager <dcreager@dcreager.net>
2022-01-06 15:38:57 -0500

    Create stack graph nodes from graph DSL nodes
    The first part of our stack graph loader creates a stack graph node for
    each graph DSL node.  You use attributes to specify the kind of stack
    graph node (defaulting to “internal”), along with any other necessary
    properties of the node.

--- tree-sitter-stack-graphs/tests/it/main.rs
+++ tree-sitter-stack-graphs/tests/it/main.rs
@@ -4,3 +4,5 @@
 // Licensed under either of Apache License, Version 2.0, or MIT license, at your option.
 // Please see the LICENSE-APACHE or LICENSE-MIT files in this distribution for license details.
 // ------------------------------------------------------------------------------------------------
+
+mod nodes;
