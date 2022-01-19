e0fba71ce2508b1b3b9a732695953a0187f72054
Hendrik van Antwerpen <hendrikvanantwerpen@github.com>
2022-01-19 16:26:23 +0100

    Add command-line interface

---
+++ tree-sitter-stack-graphs/src/bin/tree-sitter-stack-graphs/main.rs
@@ -0,0 +1,34 @@
+// -*- coding: utf-8 -*-
+// ------------------------------------------------------------------------------------------------
+// Copyright © 2021, stack-graphs authors.
+// Licensed under either of Apache License, Version 2.0, or MIT license, at your option.
+// Please see the LICENSE-APACHE or LICENSE-MIT files in this distribution for license details.
+// ------------------------------------------------------------------------------------------------
+
+use anyhow::anyhow;
+use anyhow::Result;
+use clap::AppSettings;
+use clap::Parser;
+use clap::Subcommand;
+
+#[derive(Parser)]
+#[clap(about)]
+#[clap(version)]
+#[clap(global_setting(AppSettings::PropagateVersion))]
+#[clap(global_setting(AppSettings::UseLongFormatForHelpSubcommand))]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {}
+
+#[allow(unused_variables)]
+#[allow(unreachable_code)]
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+    match &cli.command {
+        _ => Err(anyhow!("no commands defined")),
+    }
+}
