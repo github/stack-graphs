c3faf76427257df4b484b911228a6c78ace50d05
Hendrik van Antwerpen <hendrikvanantwerpen@github.com>
2022-01-21 18:59:58 +0100

    Implement test assertions

--- tree-sitter-stack-graphs/src/bin/tree-sitter-stack-graphs/test.rs
+++ tree-sitter-stack-graphs/src/bin/tree-sitter-stack-graphs/test.rs
@@ -7,12 +7,16 @@
 
 use anyhow::anyhow;
 use anyhow::Context as _;
-use anyhow::Result;
+use colored::Colorize as _;
 use stack_graphs::graph::StackGraph;
+use stack_graphs::paths::Paths;
 use std::path::Path;
 use std::path::PathBuf;
+use thiserror::Error;
 use tree_sitter_graph::ExecutionError;
 use tree_sitter_graph::Variables;
+use tree_sitter_stack_graphs::assert::Assertions;
+use tree_sitter_stack_graphs::assert::Result;
 use tree_sitter_stack_graphs::StackGraphLanguage;
 use walkdir::WalkDir;
 
@@ -24,14 +28,23 @@ pub struct Command {
     #[clap(flatten)]
     loader: LoaderArgs,
 
-    /// Source paths to analyze.
+    /// Source paths to test.
     #[clap(name = "PATHS")]
     sources: Vec<PathBuf>,
+
+    /// Hide failure errors.
+    #[clap(long)]
+    hide_failure_errors: bool,
+
+    /// Show paths for failed tests.
+    #[clap(long)]
+    show_paths_on_failure: bool,
 }
 
 impl Command {
-    pub fn run(&self) -> Result<()> {
+    pub fn run(&self) -> anyhow::Result<()> {
         let mut loader = self.loader.new_loader()?;
+        let mut total_failure_count = 0;
         for source_path in &self.sources {
             for source_entry in WalkDir::new(source_path)
                 .follow_links(true)
@@ -42,27 +55,39 @@ impl Command {
                 let source_path = source_entry.path();
 
                 match loader.load_for_source_path(source_path) {
-                    Ok(sgl) => {
-                        eprintln!("Process {}", source_path.display());
-                        if let Err(e) = self.process(sgl, source_path) {
-                            eprintln!("{:?}", e);
-                            eprintln!("Failed {}", source_path.display());
+                    Ok(sgl) => match self.process(sgl, source_path) {
+                        Err(TestError::AssertionsFailed(failure_count)) => {
+                            total_failure_count += failure_count;
                         }
-                    }
+                        r => {
+                            r?;
+                        }
+                    },
                     Err(e) => {
-                        eprintln!("{:?}", e);
-                        eprintln!("Ignored {}", source_path.display());
+                        println!("{} {}", "⦵".dimmed(), source_path.display(),);
+                        if !self.hide_failure_errors {
+                            println!("  {}", e);
+                        }
                     }
                 }
             }
         }
-        Ok(())
+        if total_failure_count == 0 {
+            Ok(())
+        } else {
+            Err(anyhow!("{} assertions failed", total_failure_count))
+        }
     }
 
-    fn process(&self, sgl: &mut StackGraphLanguage, source_path: &Path) -> Result<()> {
+    fn process(
+        &self,
+        sgl: &mut StackGraphLanguage,
+        source_path: &Path,
+    ) -> std::result::Result<(), TestError> {
         let source = std::fs::read(source_path)
             .with_context(|| format!("Error reading source file {}", source_path.display()))?;
-        let source = String::from_utf8(source)?;
+        let source = String::from_utf8(source)
+            .with_context(|| format!("Error reading source file {}", source_path.display()))?;
 
         let mut globals = Variables::new();
         globals
@@ -70,7 +95,9 @@ impl Command {
                 "FILE_PATH".into(),
                 source_path.as_os_str().to_str().unwrap().into(),
             )
-            .map_err(|_| ExecutionError::DuplicateVariable("FILE_PATH".into()))?;
+            .map_err(|_| {
+                TestError::Other(ExecutionError::DuplicateVariable("FILE_PATH".into()).into())
+            })?;
 
         let mut stack_graph = StackGraph::new();
         let file = stack_graph.get_or_create_file(source_path.to_str().unwrap());
@@ -83,6 +110,84 @@ impl Command {
                 )
             })?;
 
-        Ok(())
+        let assertions =
+            Assertions::from_source(file, &source).map_err(|e| TestError::Other(e.into()))?;
+        let mut paths = Paths::new();
+        let result = assertions.run(&stack_graph, &mut paths);
+        if result.failure_count() == 0 {
+            println!(
+                "{} {}: {}/{} assertions",
+                "✓".green(),
+                stack_graph[file],
+                result.success_count(),
+                assertions.count()
+            );
+            Ok(())
+        } else {
+            println!(
+                "{} {}: {}/{} assertions",
+                "✗".red(),
+                stack_graph[file],
+                result.success_count(),
+                assertions.count()
+            );
+            if !self.hide_failure_errors {
+                for result in &result {
+                    if let Result::Failure(e) = result {
+                        println!("  {}", e);
+                    }
+                }
+            }
+            if self.show_paths_on_failure {
+                println!("  complete paths:");
+                paths.find_all_paths(
+                    &stack_graph,
+                    stack_graph.nodes_for_file(file),
+                    |sg, ps, p| {
+                        if p.is_complete(sg) {
+                            println!("  * {}", p.display(sg, ps));
+                        }
+                    },
+                );
+                println!("  incomplete reference paths:");
+                paths.find_all_paths(
+                    &stack_graph,
+                    stack_graph.nodes_for_file(file),
+                    |sg, ps, p| {
+                        if !p.edges.is_empty()
+                            && !p.is_complete(sg)
+                            && sg[p.start_node].is_reference()
+                        {
+                            println!("  * {}", p.display(sg, ps));
+                        }
+                    },
+                );
+                println!("  incomplete definition paths:");
+                paths.find_all_paths(
+                    &stack_graph,
+                    stack_graph.nodes_for_file(file).filter(|n| {
+                        let n = &stack_graph[*n];
+                        n.is_root() || n.is_reference()
+                    }),
+                    |sg, ps, p| {
+                        if !p.edges.is_empty()
+                            && !p.is_complete(sg)
+                            && sg[p.end_node].is_definition()
+                        {
+                            println!("  * {}", p.display(sg, ps));
+                        }
+                    },
+                );
+            }
+            Err(TestError::AssertionsFailed(result.failure_count()))
+        }
     }
+}
+
+#[derive(Debug, Error)]
+pub enum TestError {
+    #[error("{0} assertions failed")]
+    AssertionsFailed(usize),
+    #[error(transparent)]
+    Other(#[from] anyhow::Error),
 }
