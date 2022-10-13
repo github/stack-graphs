99153af3658afcc12914a83af3714b2deb46cca9
rnkaufman <misskaufman@gmail.com>
2022-10-12 17:55:02 -0700

    Initial setup for stack-graphs-java crate

---
+++ stack-graphs-java/bindings/rust/lib.rs
@@ -0,0 +1,8 @@
+// -*- coding: utf-8 -*-
+// -------------------------------------------------------------------------------------
+// Copyright © 2022, stack-graphs-java authors.
+// See the LICENSE file in this repo for license details.
+// -------------------------------------------------------------------------------------
+
+//! This crate provides stack graph rules for Java for precise code navigation.
+//! More to come!
