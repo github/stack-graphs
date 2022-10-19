b7647e697a92f5ebb79f1ca7d699f0d63a1be673
Hendrik van Antwerpen <hendrikvanantwerpen@github.com>
2022-10-19 18:06:53 +0200

    Add tree-sitter-stack-graphs-typescript project

---
+++ languages/tree-sitter-stack-graphs-typescript/rust/lib.rs
@@ -0,0 +1,24 @@
+use tree_sitter_stack_graphs::loader::BuiltinsConfiguration;
+use tree_sitter_stack_graphs::loader::LanguageConfiguration;
+
+/// The stack graphs tsg source for this language
+const STACK_GRAPHS_TSG_SOURCE: &str = include_str!("../src/stack-graphs.tsg");
+
+/// The stack graphs builtins configuration for this language
+const STACK_GRAPHS_BUILTINS_CONFIG: &str = include_str!("../src/builtins.cfg");
+/// The stack graphs builtins source for this language
+const STACK_GRAPHS_BUILTINS_SOURCE: &str = include_str!("../src/builtins.ts");
+
+pub fn language_configuration() -> LanguageConfiguration {
+    LanguageConfiguration {
+        language: tree_sitter_typescript::language_typescript(),
+        scope: Some(String::from("source.ts")),
+        content_regex: None,
+        file_types: vec![String::from("ts")],
+        tsg_source: STACK_GRAPHS_TSG_SOURCE.to_string(),
+        builtins: Some(BuiltinsConfiguration {
+            source: STACK_GRAPHS_BUILTINS_SOURCE.to_string(),
+            config: STACK_GRAPHS_BUILTINS_CONFIG.to_string(),
+        }),
+    }
+}
