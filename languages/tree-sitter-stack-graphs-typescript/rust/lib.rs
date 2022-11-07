92ef01aa243d5fd8881132a92c4e59a577f8126a
Hendrik van Antwerpen <hendrikvanantwerpen@github.com>
2022-11-07 12:18:58 +0100

    Make static sources public again

--- languages/tree-sitter-stack-graphs-typescript/rust/lib.rs
+++ languages/tree-sitter-stack-graphs-typescript/rust/lib.rs
@@ -13,12 +13,12 @@ use tsconfig::TsConfigAnalyzer;
 pub mod tsconfig;
 
 /// The stack graphs tsg source for this language
-const STACK_GRAPHS_TSG_SOURCE: &str = include_str!("../src/stack-graphs.tsg");
+pub const STACK_GRAPHS_TSG_SOURCE: &str = include_str!("../src/stack-graphs.tsg");
 
 /// The stack graphs builtins configuration for this language
-const STACK_GRAPHS_BUILTINS_CONFIG: &str = include_str!("../src/builtins.cfg");
+pub const STACK_GRAPHS_BUILTINS_CONFIG: &str = include_str!("../src/builtins.cfg");
 /// The stack graphs builtins source for this language
-const STACK_GRAPHS_BUILTINS_SOURCE: &str = include_str!("../src/builtins.ts");
+pub const STACK_GRAPHS_BUILTINS_SOURCE: &str = include_str!("../src/builtins.ts");
 
 pub fn language_configuration(cancellation_flag: &dyn CancellationFlag) -> LanguageConfiguration {
     LanguageConfiguration::from_tsg_str(
