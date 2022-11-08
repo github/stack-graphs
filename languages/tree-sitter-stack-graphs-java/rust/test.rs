f7104b58d247a1f993e09c0ba8bfbf6885b96192
rnkaufman <misskaufman@gmail.com>
2022-11-08 10:37:03 -0800

    Rename test folder

--- languages/tree-sitter-stack-graphs-java/rust/test.rs
+++ languages/tree-sitter-stack-graphs-java/rust/test.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 use tree_sitter_stack_graphs::cli::CiTester;
 
 fn main() -> anyhow::Result<()> {
-    let test_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests");
+    let test_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("test");
     CiTester::new(
         vec![tree_sitter_stack_graphs_java::language_configuration()],
         vec![test_path],
