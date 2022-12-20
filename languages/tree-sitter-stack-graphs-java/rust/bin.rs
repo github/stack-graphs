992429f227174576b39cc73c2ac6edb10a73948b
rnkaufman <misskaufman@gmail.com>
2022-12-20 10:13:15 -0800

    Update bin/lib.rs with new setup after pulling main

--- languages/tree-sitter-stack-graphs-java/rust/bin.rs
+++ languages/tree-sitter-stack-graphs-java/rust/bin.rs
@@ -1,5 +1,6 @@
 use tree_sitter_stack_graphs::cli::LanguageConfigurationsCli as Cli;
+use tree_sitter_stack_graphs::NoCancellation;
 
 fn main() -> anyhow::Result<()> {
-    Cli::main(vec![tree_sitter_stack_graphs_java::language_configuration()])
+    Cli::main(vec![tree_sitter_stack_graphs_java::language_configuration(&NoCancellation)])
 }
