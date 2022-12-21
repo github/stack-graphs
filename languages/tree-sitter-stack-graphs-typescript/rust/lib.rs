549f02c5e7045eba3d0058bdb8b6323a0ce79877
Hendrik van Antwerpen <hendrikvanantwerpen@github.com>
2022-12-21 12:44:51 +0100

    Add global variable names to crate

--- languages/tree-sitter-stack-graphs-typescript/rust/lib.rs
+++ languages/tree-sitter-stack-graphs-typescript/rust/lib.rs
@@ -20,6 +20,11 @@ pub const STACK_GRAPHS_BUILTINS_CONFIG: &str = include_str!("../src/builtins.cfg
 /// The stack graphs builtins source for this language
 pub const STACK_GRAPHS_BUILTINS_SOURCE: &str = include_str!("../src/builtins.ts");
 
+/// The name of the file path global variable
+pub const FILE_PATH_VAR: &str = "FILE_PATH";
+/// The name of the project name global variable
+pub const PROJECT_NAME_VAR: &str = "PROJECT_NAME";
+
 pub fn language_configuration(cancellation_flag: &dyn CancellationFlag) -> LanguageConfiguration {
     LanguageConfiguration::from_tsg_str(
         tree_sitter_typescript::language_typescript(),
