b47c201ff42cb16084858ab92a338c7fb1273f9f
Rob Rix <rob.rix@me.com>
2023-01-09 13:41:06 -0500

    NoCancellation.

--- languages/tree-sitter-stack-graphs-java/rust/test.rs
+++ languages/tree-sitter-stack-graphs-java/rust/test.rs
@@ -1,10 +1,12 @@
 use std::path::PathBuf;
-use tree_sitter_stack_graphs::cli::CiTester;
+use tree_sitter_stack_graphs::{cli::CiTester, NoCancellation};
 
 fn main() -> anyhow::Result<()> {
     let test_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("test");
     CiTester::new(
-        vec![tree_sitter_stack_graphs_java::language_configuration()],
+        vec![tree_sitter_stack_graphs_java::language_configuration(
+            &NoCancellation,
+        )],
         vec![test_path],
     )
     .run()
