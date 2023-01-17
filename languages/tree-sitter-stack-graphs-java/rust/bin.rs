e5be80f5598f62ac82957129163c9e67722d1b7c
rnkaufman <misskaufman@gmail.com>
2023-01-17 15:42:33 -0800

    Update java rust bin.rs to account for CLI updates

--- languages/tree-sitter-stack-graphs-java/rust/bin.rs
+++ languages/tree-sitter-stack-graphs-java/rust/bin.rs
@@ -1,8 +1,17 @@
-use tree_sitter_stack_graphs::cli::LanguageConfigurationsCli as Cli;
+use clap::Parser;
+use tree_sitter_stack_graphs::cli::provided_languages::Subcommands;
 use tree_sitter_stack_graphs::NoCancellation;
 
 fn main() -> anyhow::Result<()> {
-    Cli::main(vec![tree_sitter_stack_graphs_java::language_configuration(
-        &NoCancellation,
-    )])
+    let cli = Cli::parse();
+    cli.subcommand.run(vec![
+                       tree_sitter_stack_graphs_java::language_configuration(&NoCancellation),
+    ])
+}
+
+#[derive(Parser)]
+#[clap(about, version)]
+pub struct Cli {
+    #[clap(subcommand)]
+    subcommand: Subcommands,
 }
