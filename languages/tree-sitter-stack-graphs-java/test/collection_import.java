cdedff46ec69deff25d596329ef03f556aee1adb
rnkaufman <misskaufman@gmail.com>
2023-01-17 13:40:58 -0800

    Add test + rules for collections

---
+++ languages/tree-sitter-stack-graphs-java/test/collection_import.java
@@ -0,0 +1,11 @@
+import test.Item;
+
+import java.util.Collection;
+
+public class CollectionImport {
+  public void main() {
+    Collection<Item> items = new ArrayList<>();
+    // ^ defined: 3
+            // ^ defined: 1
+  }
+}
