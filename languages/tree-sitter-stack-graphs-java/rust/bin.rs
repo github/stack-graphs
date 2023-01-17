a9371e8b48df4a725c3687cf281c352df58ca17f
rnkaufman <misskaufman@gmail.com>
2023-01-17 15:00:39 -0800

    Fix cargo fmt check

--- languages/tree-sitter-stack-graphs-java/rust/bin.rs
+++ languages/tree-sitter-stack-graphs-java/rust/bin.rs
@@ -2,5 +2,7 @@ use tree_sitter_stack_graphs::cli::LanguageConfigurationsCli as Cli;
 use tree_sitter_stack_graphs::NoCancellation;
 
 fn main() -> anyhow::Result<()> {
-    Cli::main(vec![tree_sitter_stack_graphs_java::language_configuration(&NoCancellation)])
+    Cli::main(vec![tree_sitter_stack_graphs_java::language_configuration(
+        &NoCancellation,
+    )])
 }
