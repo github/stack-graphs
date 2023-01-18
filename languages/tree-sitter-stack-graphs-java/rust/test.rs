ab79b6b2dc7d712e19786161c9ca34fd13019adb
rnkaufman <misskaufman@gmail.com>
2023-01-17 16:39:34 -0800

    Use new 'Tester' instead of CiTester

--- languages/tree-sitter-stack-graphs-java/rust/test.rs
+++ languages/tree-sitter-stack-graphs-java/rust/test.rs
@@ -1,9 +1,9 @@
 use std::path::PathBuf;
-use tree_sitter_stack_graphs::{cli::CiTester, NoCancellation};
+use tree_sitter_stack_graphs::{ci::Tester, NoCancellation};
 
 fn main() -> anyhow::Result<()> {
     let test_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("test");
-    CiTester::new(
+    Tester::new(
         vec![tree_sitter_stack_graphs_java::language_configuration(
             &NoCancellation,
         )],
