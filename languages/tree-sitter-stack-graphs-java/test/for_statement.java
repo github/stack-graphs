270ed965691e5f6ab8bc2564fab83301a737e6f8
Rob Rix <rob.rix@me.com>
2023-01-25 10:03:42 -0500

    Improve for statement tests.

--- languages/tree-sitter-stack-graphs-java/test/for_statement.java
+++ languages/tree-sitter-stack-graphs-java/test/for_statement.java
@@ -1,10 +1,10 @@
 class A {
   void f() {
     boolean x;
-    for(x = false; x; x) {
+    for(x; x; x) {
       //^ defined: 3
-      //           ^ defined: 3
-      //              ^ defined: 3
+      //   ^ defined: 3
+      //      ^ defined: 3
         f();
       //^ defined: 2
     }
