16bc806a7db3d073db48c55d1a5a4d3d431afd18
rnkaufman <misskaufman@gmail.com>
2023-03-01 15:56:34 -0800

    Add test for interface types

--- languages/tree-sitter-stack-graphs-java/test/decl/interface.java
+++ languages/tree-sitter-stack-graphs-java/test/decl/interface.java
@@ -5,3 +5,8 @@ class A implements I {}
 //                 ^ defined: 1
 interface J extends I {}
 //                  ^ defined: 1
+
+interface Iterator<E> {
+    public E next() {}
+    //     ^ defined: 9
+}
