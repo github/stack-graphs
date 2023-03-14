1f2cb0a95519f2a8a7a597052bc6846c2f73b4b2
Hendrik van Antwerpen <hendrikvanantwerpen@github.com>
2023-03-14 11:49:36 +0100

    Run arena tests against both implementations

--- stack-graphs/tests/it/arena.rs
+++ stack-graphs/tests/it/arena.rs
@@ -31,6 +31,20 @@ fn can_allocate_in_arena() {
 }
 
 #[test]
+fn can_allocate_in_interning_arena() {
+    let mut arena = HashArena::new();
+    let hello1 = arena.add("hello".to_string());
+    let hello2 = arena.add("hello".to_string());
+    let there = arena.add("there".to_string());
+    assert_eq!(hello1, hello2);
+    assert_ne!(hello1, there);
+    assert_ne!(hello2, there);
+    assert_eq!(arena.get(hello1), arena.get(hello2));
+    assert_ne!(arena.get(hello1), arena.get(there));
+    assert_ne!(arena.get(hello2), arena.get(there));
+}
+
+#[test]
 fn can_allocate_in_supplemental_arena() {
     let mut arena = VecArena::<u32>::new();
     let h1 = arena.add(1);
@@ -50,43 +64,48 @@ fn can_create_lists() {
     fn collect(list: &List<u32>, arena: &impl ListArena<u32>) -> Vec<u32> {
         list.iter(arena).copied().collect()
     }
-
-    let mut arena = List::new_arena();
-    let mut list = List::empty();
-    assert_eq!(collect(&list, &arena), vec![] as Vec<u32>);
-    list.push_front(&mut arena, 1);
-    assert_eq!(collect(&list, &arena), vec![1]);
-    list.push_front(&mut arena, 2);
-    list.push_front(&mut arena, 3);
-    assert_eq!(collect(&list, &arena), vec![3, 2, 1]);
+    fn run(mut arena: impl ListArena<u32>) {
+        let mut list = List::empty();
+        assert_eq!(collect(&list, &arena), vec![] as Vec<u32>);
+        list.push_front(&mut arena, 1);
+        assert_eq!(collect(&list, &arena), vec![1]);
+        list.push_front(&mut arena, 2);
+        list.push_front(&mut arena, 3);
+        assert_eq!(collect(&list, &arena), vec![3, 2, 1]);
+    }
+    run(VecArena::new());
+    run(HashArena::new());
 }
 
 #[test]
 fn can_compare_lists() {
-    use std::cmp::Ordering;
-    let mut arena = List::new_arena();
-    let mut from_slice = |slice: &[u32]| {
-        let mut list = List::empty();
-        for element in slice.iter().rev() {
-            list.push_front(&mut arena, *element);
-        }
-        list
-    };
-    let list0 = from_slice(&[]);
-    let list1 = from_slice(&[1]);
-    let list2 = from_slice(&[2]);
-    let list12 = from_slice(&[1, 2]);
-    assert!(list0.equals(&arena, list0));
-    assert_eq!(list0.cmp(&arena, list0), Ordering::Equal);
-    assert!(!list0.equals(&arena, list1));
-    assert_eq!(list0.cmp(&arena, list1), Ordering::Less);
-    assert!(list1.equals(&arena, list1));
-    assert_eq!(list1.cmp(&arena, list1), Ordering::Equal);
-    assert!(!list1.equals(&arena, list2));
-    assert_eq!(list1.cmp(&arena, list2), Ordering::Less);
-    assert!(list2.equals(&arena, list2));
-    assert_eq!(list2.cmp(&arena, list12), Ordering::Greater);
-    assert_eq!(list1.cmp(&arena, list12), Ordering::Less);
+    fn run(mut arena: impl ListArena<u32>) {
+        use std::cmp::Ordering;
+        let mut from_slice = |slice: &[u32]| {
+            let mut list = List::empty();
+            for element in slice.iter().rev() {
+                list.push_front(&mut arena, *element);
+            }
+            list
+        };
+        let list0 = from_slice(&[]);
+        let list1 = from_slice(&[1]);
+        let list2 = from_slice(&[2]);
+        let list12 = from_slice(&[1, 2]);
+        assert!(list0.equals(&arena, list0));
+        assert_eq!(list0.cmp(&arena, list0), Ordering::Equal);
+        assert!(!list0.equals(&arena, list1));
+        assert_eq!(list0.cmp(&arena, list1), Ordering::Less);
+        assert!(list1.equals(&arena, list1));
+        assert_eq!(list1.cmp(&arena, list1), Ordering::Equal);
+        assert!(!list1.equals(&arena, list2));
+        assert_eq!(list1.cmp(&arena, list2), Ordering::Less);
+        assert!(list2.equals(&arena, list2));
+        assert_eq!(list2.cmp(&arena, list12), Ordering::Greater);
+        assert_eq!(list1.cmp(&arena, list12), Ordering::Less);
+    }
+    run(VecArena::new());
+    run(HashArena::new());
 }
 
 #[test]
@@ -94,56 +113,61 @@ fn can_create_reversible_lists() {
     fn collect(list: &ReversibleList<u32>, arena: &impl ReversibleListArena<u32>) -> Vec<u32> {
         list.iter(arena).copied().collect()
     }
-
-    let mut arena = ReversibleList::new_arena();
-    let mut list = ReversibleList::empty();
-    assert_eq!(collect(&list, &arena), vec![] as Vec<u32>);
-    list.push_front(&mut arena, 1);
-    assert_eq!(collect(&list, &arena), vec![1]);
-    list.push_front(&mut arena, 2);
-    list.push_front(&mut arena, 3);
-    assert_eq!(collect(&list, &arena), vec![3, 2, 1]);
-    list.reverse(&mut arena);
-    assert_eq!(collect(&list, &arena), vec![1, 2, 3]);
-    list.push_front(&mut arena, 4);
-    list.push_front(&mut arena, 5);
-    assert_eq!(collect(&list, &arena), vec![5, 4, 1, 2, 3]);
-    list.reverse(&mut arena);
-    assert_eq!(collect(&list, &arena), vec![3, 2, 1, 4, 5]);
-    // Verify that we stash away the re-reversal so that we don't have to recompute it.
-    assert!(list.have_reversal(&arena));
+    fn run(mut arena: impl ReversibleListArena<u32>) {
+        let mut list = ReversibleList::empty();
+        assert_eq!(collect(&list, &arena), vec![] as Vec<u32>);
+        list.push_front(&mut arena, 1);
+        assert_eq!(collect(&list, &arena), vec![1]);
+        list.push_front(&mut arena, 2);
+        list.push_front(&mut arena, 3);
+        assert_eq!(collect(&list, &arena), vec![3, 2, 1]);
+        list.reverse(&mut arena);
+        assert_eq!(collect(&list, &arena), vec![1, 2, 3]);
+        list.push_front(&mut arena, 4);
+        list.push_front(&mut arena, 5);
+        assert_eq!(collect(&list, &arena), vec![5, 4, 1, 2, 3]);
+        list.reverse(&mut arena);
+        assert_eq!(collect(&list, &arena), vec![3, 2, 1, 4, 5]);
+        // Verify that we stash away the re-reversal so that we don't have to recompute it.
+        assert!(list.have_reversal(&arena));
+    }
+    run(VecArena::new());
+    run(HashArena::new());
 }
 
 #[test]
 fn can_compare_reversible_lists() {
     use std::cmp::Ordering;
-    let mut arena = ReversibleList::new_arena();
-    let mut from_slice = |slice: &[u32]| {
-        let mut list = ReversibleList::empty();
-        for element in slice.iter().rev() {
-            list.push_front(&mut arena, *element);
-        }
-        list
-    };
-    let list0 = from_slice(&[]);
-    let list1 = from_slice(&[1]);
-    let list2 = from_slice(&[2]);
-    let list12 = from_slice(&[1, 2]);
-    assert!(list0.equals(&arena, list0));
-    assert_eq!(list0.cmp(&arena, list0), Ordering::Equal);
-    assert!(!list0.equals(&arena, list1));
-    assert_eq!(list0.cmp(&arena, list1), Ordering::Less);
-    assert!(list1.equals(&arena, list1));
-    assert_eq!(list1.cmp(&arena, list1), Ordering::Equal);
-    assert!(!list1.equals(&arena, list2));
-    assert_eq!(list1.cmp(&arena, list2), Ordering::Less);
-    assert!(list2.equals(&arena, list2));
-    assert_eq!(list2.cmp(&arena, list12), Ordering::Greater);
-    assert_eq!(list1.cmp(&arena, list12), Ordering::Less);
-    let mut list21 = list12;
-    list21.reverse(&mut arena);
-    assert_eq!(list2.cmp(&arena, list21), Ordering::Less);
-    assert_eq!(list1.cmp(&arena, list21), Ordering::Less);
+    fn run(mut arena: impl ReversibleListArena<u32>) {
+        let mut from_slice = |slice: &[u32]| {
+            let mut list = ReversibleList::empty();
+            for element in slice.iter().rev() {
+                list.push_front(&mut arena, *element);
+            }
+            list
+        };
+        let list0 = from_slice(&[]);
+        let list1 = from_slice(&[1]);
+        let list2 = from_slice(&[2]);
+        let list12 = from_slice(&[1, 2]);
+        assert!(list0.equals(&arena, list0));
+        assert_eq!(list0.cmp(&arena, list0), Ordering::Equal);
+        assert!(!list0.equals(&arena, list1));
+        assert_eq!(list0.cmp(&arena, list1), Ordering::Less);
+        assert!(list1.equals(&arena, list1));
+        assert_eq!(list1.cmp(&arena, list1), Ordering::Equal);
+        assert!(!list1.equals(&arena, list2));
+        assert_eq!(list1.cmp(&arena, list2), Ordering::Less);
+        assert!(list2.equals(&arena, list2));
+        assert_eq!(list2.cmp(&arena, list12), Ordering::Greater);
+        assert_eq!(list1.cmp(&arena, list12), Ordering::Less);
+        let mut list21 = list12;
+        list21.reverse(&mut arena);
+        assert_eq!(list2.cmp(&arena, list21), Ordering::Less);
+        assert_eq!(list1.cmp(&arena, list21), Ordering::Less);
+    }
+    run(VecArena::new());
+    run(HashArena::new());
 }
 
 #[test]
@@ -157,39 +181,40 @@ fn can_create_deques() {
     fn collect_rev(deque: &Deque<u32>, arena: &mut impl DequeArena<u32>) -> Vec<u32> {
         deque.iter_reversed(arena).copied().collect()
     }
-
-    let mut arena = Deque::new_arena();
-    let mut deque = Deque::empty();
-    assert_eq!(collect(&deque, &mut arena), vec![] as Vec<u32>);
-    assert_eq!(collect_rev(&deque, &mut arena), vec![] as Vec<u32>);
-    deque.push_front(&mut arena, 1);
-    assert_eq!(collect(&deque, &mut arena), vec![1]);
-    assert_eq!(collect_rev(&deque, &mut arena), vec![1]);
-    deque.push_front(&mut arena, 2);
-    deque.push_front(&mut arena, 3);
-    assert_eq!(collect(&deque, &mut arena), vec![3, 2, 1]);
-    assert_eq!(collect_rev(&deque, &mut arena), vec![1, 2, 3]);
-    deque.push_back(&mut arena, 4);
-    assert_eq!(collect(&deque, &mut arena), vec![3, 2, 1, 4]);
-    assert_eq!(collect_rev(&deque, &mut arena), vec![4, 1, 2, 3]);
-    deque.push_back(&mut arena, 5);
-    deque.push_back(&mut arena, 6);
-    assert_eq!(collect(&deque, &mut arena), vec![3, 2, 1, 4, 5, 6]);
-    assert_eq!(collect_rev(&deque, &mut arena), vec![6, 5, 4, 1, 2, 3]);
-    deque.push_front(&mut arena, 7);
-    deque.push_front(&mut arena, 8);
-    assert_eq!(collect(&deque, &mut arena), vec![8, 7, 3, 2, 1, 4, 5, 6]);
-    assert_eq!(collect_reused(&deque, &arena), vec![8, 7, 3, 2, 1, 4, 5, 6]);
-    assert_eq!(
-        collect_rev(&deque, &mut arena),
-        vec![6, 5, 4, 1, 2, 3, 7, 8]
-    );
+    fn run(mut arena: impl ReversibleListArena<u32>) {
+        let mut deque = Deque::empty();
+        assert_eq!(collect(&deque, &mut arena), vec![] as Vec<u32>);
+        assert_eq!(collect_rev(&deque, &mut arena), vec![] as Vec<u32>);
+        deque.push_front(&mut arena, 1);
+        assert_eq!(collect(&deque, &mut arena), vec![1]);
+        assert_eq!(collect_rev(&deque, &mut arena), vec![1]);
+        deque.push_front(&mut arena, 2);
+        deque.push_front(&mut arena, 3);
+        assert_eq!(collect(&deque, &mut arena), vec![3, 2, 1]);
+        assert_eq!(collect_rev(&deque, &mut arena), vec![1, 2, 3]);
+        deque.push_back(&mut arena, 4);
+        assert_eq!(collect(&deque, &mut arena), vec![3, 2, 1, 4]);
+        assert_eq!(collect_rev(&deque, &mut arena), vec![4, 1, 2, 3]);
+        deque.push_back(&mut arena, 5);
+        deque.push_back(&mut arena, 6);
+        assert_eq!(collect(&deque, &mut arena), vec![3, 2, 1, 4, 5, 6]);
+        assert_eq!(collect_rev(&deque, &mut arena), vec![6, 5, 4, 1, 2, 3]);
+        deque.push_front(&mut arena, 7);
+        deque.push_front(&mut arena, 8);
+        assert_eq!(collect(&deque, &mut arena), vec![8, 7, 3, 2, 1, 4, 5, 6]);
+        assert_eq!(collect_reused(&deque, &arena), vec![8, 7, 3, 2, 1, 4, 5, 6]);
+        assert_eq!(
+            collect_rev(&deque, &mut arena),
+            vec![6, 5, 4, 1, 2, 3, 7, 8]
+        );
+    }
+    run(VecArena::new());
+    run(HashArena::new());
 }
 
 #[test]
 fn can_compare_deques() {
     use std::cmp::Ordering;
-    let mut arena = Deque::new_arena();
     // Build up deques in both directions so that our comparisons have to test the "reverse if
     // needed" logic.
     fn from_slice_forwards(slice: &[u32], arena: &mut impl DequeArena<u32>) -> Deque<u32> {
@@ -206,42 +231,32 @@ fn can_compare_deques() {
         }
         deque
     }
-    let deque0 = from_slice_forwards(&[], &mut arena);
-    let mut deque1 = from_slice_backwards(&[1], &mut arena);
-    let deque2 = from_slice_forwards(&[2], &mut arena);
-    let mut deque10 = from_slice_backwards(&[1, 0], &mut arena);
-    let deque12 = from_slice_backwards(&[1, 2], &mut arena);
-    assert!(deque0.equals(&mut arena, deque0));
-    assert_eq!(deque0.cmp(&mut arena, deque0), Ordering::Equal);
-    assert!(!deque0.equals(&mut arena, deque1));
-    assert_eq!(deque0.cmp(&mut arena, deque1), Ordering::Less);
-    assert!(deque1.equals(&mut arena, deque1));
-    assert_eq!(deque1.cmp(&mut arena, deque1), Ordering::Equal);
-    assert!(!deque1.equals(&mut arena, deque2));
-    assert_eq!(deque1.cmp(&mut arena, deque2), Ordering::Less);
-    assert!(deque2.equals(&mut arena, deque2));
-    assert_eq!(deque2.cmp(&mut arena, deque12), Ordering::Greater);
-    assert_eq!(deque1.cmp(&mut arena, deque12), Ordering::Less);
+    fn run(mut arena: impl ReversibleListArena<u32>) {
+        let deque0 = from_slice_forwards(&[], &mut arena);
+        let mut deque1 = from_slice_backwards(&[1], &mut arena);
+        let deque2 = from_slice_forwards(&[2], &mut arena);
+        let mut deque10 = from_slice_backwards(&[1, 0], &mut arena);
+        let deque12 = from_slice_backwards(&[1, 2], &mut arena);
+        assert!(deque0.equals(&mut arena, deque0));
+        assert_eq!(deque0.cmp(&mut arena, deque0), Ordering::Equal);
+        assert!(!deque0.equals(&mut arena, deque1));
+        assert_eq!(deque0.cmp(&mut arena, deque1), Ordering::Less);
+        assert!(deque1.equals(&mut arena, deque1));
+        assert_eq!(deque1.cmp(&mut arena, deque1), Ordering::Equal);
+        assert!(!deque1.equals(&mut arena, deque2));
+        assert_eq!(deque1.cmp(&mut arena, deque2), Ordering::Less);
+        assert!(deque2.equals(&mut arena, deque2));
+        assert_eq!(deque2.cmp(&mut arena, deque12), Ordering::Greater);
+        assert_eq!(deque1.cmp(&mut arena, deque12), Ordering::Less);
 
-    // We should get the same result regardless of which direction the deques are pointing.
-    deque1.ensure_forwards(&mut arena);
-    deque10.ensure_forwards(&mut arena);
-    assert_eq!(deque1.cmp(&mut arena, deque10), Ordering::Less);
-    deque1.ensure_backwards(&mut arena);
-    deque10.ensure_backwards(&mut arena);
-    assert_eq!(deque1.cmp(&mut arena, deque10), Ordering::Less);
-}
-
-#[test]
-fn can_allocate_in_interning_arena() {
-    let mut arena = HashArena::new();
-    let hello1 = arena.add("hello".to_string());
-    let hello2 = arena.add("hello".to_string());
-    let there = arena.add("there".to_string());
-    assert_eq!(hello1, hello2);
-    assert_ne!(hello1, there);
-    assert_ne!(hello2, there);
-    assert_eq!(arena.get(hello1), arena.get(hello2));
-    assert_ne!(arena.get(hello1), arena.get(there));
-    assert_ne!(arena.get(hello2), arena.get(there));
+        // We should get the same result regardless of which direction the deques are pointing.
+        deque1.ensure_forwards(&mut arena);
+        deque10.ensure_forwards(&mut arena);
+        assert_eq!(deque1.cmp(&mut arena, deque10), Ordering::Less);
+        deque1.ensure_backwards(&mut arena);
+        deque10.ensure_backwards(&mut arena);
+        assert_eq!(deque1.cmp(&mut arena, deque10), Ordering::Less);
+    }
+    run(VecArena::new());
+    run(HashArena::new());
 }
