29c3cc596631208d1f85972893d98cbc9e9bef50
Hendrik van Antwerpen <hendrikvanantwerpen@github.com>
2023-03-21 15:51:27 +0100

    Fix copyright years

--- stack-graphs/src/serde/stitching.rs
+++ stack-graphs/src/serde/stitching.rs
@@ -1,6 +1,6 @@
 // -*- coding: utf-8 -*-
 // ------------------------------------------------------------------------------------------------
-// Copyright © 2021, stack-graphs authors.
+// Copyright © 2023, stack-graphs authors.
 // Licensed under either of Apache License, Version 2.0, or MIT license, at your option.
 // Please see the LICENSE-APACHE or LICENSE-MIT files in this distribution for license details.
 // ------------------------------------------------------------------------------------------------
