d558b052e3b755a1938e62871fb56c6cb4213124
Hendrik van Antwerpen <hendrikvanantwerpen@github.com>
2023-03-21 13:18:54 +0100

    Remove unused serialization code

--- stack-graphs/src/json.rs
+++ stack-graphs/src/json.rs
@@ -5,17 +5,9 @@
 // Please see the LICENSE-APACHE or LICENSE-MIT files in this distribution for license details.
 // ------------------------------------------------------------------------------------------------
 
-use serde::ser::Serialize;
-use serde::ser::SerializeSeq;
-use serde::ser::SerializeStruct;
-use serde::ser::Serializer;
 use serde_json::Value;
-use std::ops::Index;
 use thiserror::Error;
 
-use crate::arena::Handle;
-use crate::graph::File;
-use crate::graph::NodeID;
 use crate::graph::StackGraph;
 use crate::partial::PartialPaths;
 pub use crate::serde::Filter;
@@ -26,72 +18,6 @@ use crate::stitching::Database;
 #[derive(Debug, Error)]
 #[error(transparent)]
 pub struct JsonError(#[from] serde_json::error::Error);
-
-//-----------------------------------------------------------------------------
-// InStackGraph
-
-struct InStackGraph<'a, T>(&'a StackGraph, T, &'a dyn Filter);
-
-impl<'a, T> InStackGraph<'a, T> {
-    fn with<U>(&'a self, u: U) -> InStackGraph<'a, U> {
-        InStackGraph(self.0, u, self.2)
-    }
-
-    fn with_idx<Idx: Copy, U: ?Sized>(&'a self, idx: Idx) -> InStackGraph<'a, (Idx, &U)>
-    where
-        StackGraph: Index<Idx, Output = U>,
-    {
-        InStackGraph(self.0, (idx, &self.0[idx]), self.2)
-    }
-}
-
-//-----------------------------------------------------------------------------
-// Files
-
-impl Serialize for InStackGraph<'_, (Handle<File>, &File)> {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let file = self.1 .1;
-        serializer.serialize_str(file.name())
-    }
-}
-
-//-----------------------------------------------------------------------------
-// Nodes
-
-impl Serialize for InStackGraph<'_, &Vec<NodeID>> {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let nodes = self.1;
-
-        let mut ser = serializer.serialize_seq(nodes.len().into())?;
-        for node in nodes {
-            ser.serialize_element(&self.with(node))?;
-        }
-        ser.end()
-    }
-}
-
-impl Serialize for InStackGraph<'_, &NodeID> {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let node_id = self.1;
-
-        let len = 1 + node_id.file().map(|_| 1).unwrap_or(0);
-        let mut ser = serializer.serialize_struct("node_id", len)?;
-        if let Some(file) = node_id.file() {
-            ser.serialize_field("file", &self.with_idx(file))?;
-        }
-        ser.serialize_field("local_id", &node_id.local_id())?;
-        ser.end()
-    }
-}
 
 //-----------------------------------------------------------------------------
 // Database
