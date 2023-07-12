f8009e10bb86e04a7876745350cc55657a9391ce
Hendrik van Antwerpen <hendrikvanantwerpen@github.com>
2023-07-12 18:59:30 +0200

    Add definiens_node and syntax_type attributes

--- tree-sitter-stack-graphs/src/lib.rs
+++ tree-sitter-stack-graphs/src/lib.rs
@@ -364,6 +364,7 @@ static SCOPE_TYPE: &'static str = "scope";
 
 // Node attribute names
 static DEBUG_ATTR_PREFIX: &'static str = "debug_";
+static DEFINIENS_NODE_ATTR: &'static str = "definiens_node";
 static EMPTY_SOURCE_SPAN_ATTR: &'static str = "empty_source_span";
 static IS_DEFINITION_ATTR: &'static str = "is_definition";
 static IS_ENDPOINT_ATTR: &'static str = "is_endpoint";
@@ -372,13 +373,28 @@ static IS_REFERENCE_ATTR: &'static str = "is_reference";
 static SCOPE_ATTR: &'static str = "scope";
 static SOURCE_NODE_ATTR: &'static str = "source_node";
 static SYMBOL_ATTR: &'static str = "symbol";
+static SYNTAX_TYPE_ATTR: &'static str = "syntax_type";
 static TYPE_ATTR: &'static str = "type";
 
 // Expected attributes per node type
-static POP_SCOPED_SYMBOL_ATTRS: Lazy<HashSet<&'static str>> =
-    Lazy::new(|| HashSet::from([TYPE_ATTR, SYMBOL_ATTR, IS_DEFINITION_ATTR]));
-static POP_SYMBOL_ATTRS: Lazy<HashSet<&'static str>> =
-    Lazy::new(|| HashSet::from([TYPE_ATTR, SYMBOL_ATTR, IS_DEFINITION_ATTR]));
+static POP_SCOPED_SYMBOL_ATTRS: Lazy<HashSet<&'static str>> = Lazy::new(|| {
+    HashSet::from([
+        TYPE_ATTR,
+        SYMBOL_ATTR,
+        IS_DEFINITION_ATTR,
+        DEFINIENS_NODE_ATTR,
+        SYNTAX_TYPE_ATTR,
+    ])
+});
+static POP_SYMBOL_ATTRS: Lazy<HashSet<&'static str>> = Lazy::new(|| {
+    HashSet::from([
+        TYPE_ATTR,
+        SYMBOL_ATTR,
+        IS_DEFINITION_ATTR,
+        DEFINIENS_NODE_ATTR,
+        SYNTAX_TYPE_ATTR,
+    ])
+});
 static PUSH_SCOPED_SYMBOL_ATTRS: Lazy<HashSet<&'static str>> =
     Lazy::new(|| HashSet::from([TYPE_ATTR, SYMBOL_ATTR, SCOPE_ATTR, IS_REFERENCE_ATTR]));
 static PUSH_SYMBOL_ATTRS: Lazy<HashSet<&'static str>> =
@@ -881,7 +897,7 @@ impl<'a> Builder<'a> {
                 NodeType::PushSymbol => self.load_push_symbol(node_ref)?,
                 NodeType::Scope => self.load_scope(node_ref)?,
             };
-            self.load_span(node_ref, handle)?;
+            self.load_source_info(node_ref, handle)?;
             self.load_node_debug_info(node_ref, handle)?;
         }
 
@@ -1087,28 +1103,43 @@ impl<'a> Builder<'a> {
         }
     }
 
-    fn load_span(
+    fn load_source_info(
         &mut self,
         node_ref: GraphNodeRef,
         node_handle: Handle<Node>,
     ) -> Result<(), BuildError> {
         let node = &self.graph[node_ref];
-        let source_node = match node.attributes.get(SOURCE_NODE_ATTR) {
-            Some(source_node) => &self.graph[source_node.as_syntax_node_ref()?],
-            None => return Ok(()),
-        };
-        let mut span = self.span_calculator.for_node(source_node);
-        if match node.attributes.get(EMPTY_SOURCE_SPAN_ATTR) {
-            Some(empty_source_span) => empty_source_span.as_boolean()?,
-            None => false,
-        } {
-            span.end = span.start.clone();
+
+        if let Some(source_node) = node.attributes.get(SOURCE_NODE_ATTR) {
+            let source_node = &self.graph[source_node.as_syntax_node_ref()?];
+            let mut source_span = self.span_calculator.for_node(source_node);
+            if match node.attributes.get(EMPTY_SOURCE_SPAN_ATTR) {
+                Some(empty_source_span) => empty_source_span.as_boolean()?,
+                None => false,
+            } {
+                source_span.end = source_span.start.clone();
+            }
+            let containing_line = &self.source[source_span.start.containing_line.clone()];
+            let containing_line = self.stack_graph.add_string(containing_line);
+            let source_info = self.stack_graph.source_info_mut(node_handle);
+            source_info.span = source_span;
+            source_info.containing_line = ControlledOption::some(containing_line);
         }
-        let containing_line = &self.source[span.start.containing_line.clone()];
-        let containing_line = self.stack_graph.add_string(containing_line);
-        let source_info = self.stack_graph.source_info_mut(node_handle);
-        source_info.span = span;
-        source_info.containing_line = ControlledOption::some(containing_line);
+
+        if let Some(syntax_type) = node.attributes.get(SYNTAX_TYPE_ATTR) {
+            let syntax_type = syntax_type.as_str()?;
+            let syntax_type = self.stack_graph.add_string(syntax_type);
+            let source_info = self.stack_graph.source_info_mut(node_handle);
+            source_info.syntax_type = syntax_type.into();
+        }
+
+        if let Some(definiens_node) = node.attributes.get(DEFINIENS_NODE_ATTR) {
+            let definiens_node = &self.graph[definiens_node.as_syntax_node_ref()?];
+            let definiens_span = self.span_calculator.for_node(definiens_node);
+            let source_info = self.stack_graph.source_info_mut(node_handle);
+            source_info.definiens_span = definiens_span;
+        }
+
         Ok(())
     }
 
