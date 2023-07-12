9e04e6029c958564eca84e868223bab4ff3d1206
Hendrik van Antwerpen <hendrikvanantwerpen@github.com>
2023-07-12 17:50:03 +0200

    Fix test expectation

--- languages/tree-sitter-stack-graphs-javascript/test/old/functions_and_calls/call_arguments.js
+++ languages/tree-sitter-stack-graphs-javascript/test/old/functions_and_calls/call_arguments.js
@@ -2,6 +2,6 @@ let x = 1;
 let y = 2;
 
 let z = foo(x, y++, y);
-//          ^ defined: 1, 4
-//             ^ defined: 2, 4, 4
-//                  ^ defined: 2, 4, 4
+//          ^ defined: 1
+//             ^ defined: 2
+//                  ^ defined: 2, 4
