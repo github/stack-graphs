5823da7f17e0a1d4d1a9e94754f4f0959abc449c
Rebecca Valentine <beka@gothdyke.mom>
2023-07-27 16:25:36 -0700

    Adds class expression and class declaration tests

---
+++ languages/tree-sitter-stack-graphs-javascript/test/expressions/class.js
@@ -0,0 +1,36 @@
+let x = 1;
+
+// Flow In
+
+(class extends x {
+    //         ^ defined: 1
+    z = x;
+    //  ^ defined: 1
+
+    bar() {
+        /**/ x;
+        //   ^ defined: 1
+
+        /**/ z;
+        //   ^ defined:
+        // z should not be defined here
+
+        this.z;
+        //   !!!! TODO defined: 7
+    }
+});
+
+// Flow Out
+
+(class {
+    y = 1;
+});
+
+/**/ y;
+//   ^ defined:
+// y should not be defined here
+
+// Flow Around
+
+/**/ x;
+//   ^ defined: 1
