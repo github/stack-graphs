fe94270aa56b7d0245219a179b3840d4a329b0b1
Hendrik van Antwerpen <hendrikvanantwerpen@github.com>
2023-10-04 20:06:57 +0200

    Add package.json analyzer

--- languages/tree-sitter-stack-graphs-javascript/rust/lib.rs
+++ languages/tree-sitter-stack-graphs-javascript/rust/lib.rs
@@ -10,6 +10,11 @@ use tree_sitter_stack_graphs::loader::LanguageConfiguration;
 use tree_sitter_stack_graphs::loader::LoadError;
 use tree_sitter_stack_graphs::CancellationFlag;
 
+use crate::npm_package::NpmPackageAnalyzer;
+
+mod npm_package;
+mod util;
+
 /// The stack graphs tsg source for this language.
 pub const STACK_GRAPHS_TSG_PATH: &str = "src/stack-graphs.tsg";
 /// The stack graphs tsg source for this language.
@@ -24,6 +29,7 @@ pub const STACK_GRAPHS_BUILTINS_SOURCE: &str = include_str!("../src/builtins.js"
 
 /// The name of the file path global variable.
 pub const FILE_PATH_VAR: &str = "FILE_PATH";
+pub const PROJECT_NAME_VAR: &str = "PROJECT_NAME";
 
 pub fn language_configuration(cancellation_flag: &dyn CancellationFlag) -> LanguageConfiguration {
     try_language_configuration(cancellation_flag).unwrap_or_else(|err| panic!("{}", err))
@@ -44,7 +50,7 @@ pub fn try_language_configuration(
             STACK_GRAPHS_BUILTINS_SOURCE,
         )),
         Some(STACK_GRAPHS_BUILTINS_CONFIG),
-        FileAnalyzers::new(),
+        FileAnalyzers::new().add("package.json".to_string(), NpmPackageAnalyzer {}),
         cancellation_flag,
     )
 }
---
+++ languages/tree-sitter-stack-graphs-javascript/rust/npm_package.rs
@@ -0,0 +1,119 @@
+// -*- coding: utf-8 -*-
+// ------------------------------------------------------------------------------------------------
+// Copyright © 2022, stack-graphs authors.
+// Licensed under either of Apache License, Version 2.0, or MIT license, at your option.
+// Please see the LICENSE-APACHE or LICENSE-MIT files in this distribution for license details.
+// ------------------------------------------------------------------------------------------------
+
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::path::Path;
+use std::path::PathBuf;
+
+use stack_graphs::arena::Handle;
+use stack_graphs::graph::File;
+use stack_graphs::graph::StackGraph;
+use tree_sitter_stack_graphs::BuildError;
+use tree_sitter_stack_graphs::FileAnalyzer;
+
+use crate::util::*;
+
+pub struct NpmPackageAnalyzer {}
+
+impl FileAnalyzer for NpmPackageAnalyzer {
+    fn build_stack_graph_into<'a>(
+        &self,
+        graph: &mut StackGraph,
+        file: Handle<File>,
+        _path: &Path,
+        source: &str,
+        _all_paths: &mut dyn Iterator<Item = &'a Path>,
+        globals: &HashMap<String, String>,
+        _cancellation_flag: &dyn tree_sitter_stack_graphs::CancellationFlag,
+    ) -> Result<(), tree_sitter_stack_graphs::BuildError> {
+        // read globals
+        let pkg_internal_name = globals
+            .get(crate::PROJECT_NAME_VAR)
+            .map(String::as_str)
+            .unwrap_or_default();
+
+        // parse source
+        let npm_pkg: NpmPackage =
+            serde_json::from_str(source).map_err(|_| BuildError::ParseError)?;
+
+        let root = StackGraph::root_node();
+
+        // reach package internals from root
+        //
+        //     [root] -> [pop "GUARD:PKG_INTERNAL"] -> [pop pkg_internal_name]
+        //
+        let pkg_internal_guard_pop = add_pop(graph, file, root, PKG_INTERNAL_GUARD, "");
+        let pkg_internal_name_pop =
+            add_pop(graph, file, pkg_internal_guard_pop, pkg_internal_name, "");
+
+        // reach package internals via root
+        //
+        //     [push pkg_internal_name] -> [push "GUARD:PKG_INTERNAL"] -> [root]
+        //
+        let pkg_internal_guard_push = add_push(graph, file, root, PKG_INTERNAL_GUARD, "");
+        let pkg_internal_name_push =
+            add_push(graph, file, pkg_internal_guard_push, pkg_internal_name, "");
+
+        // reach exports via package name
+        //
+        //     [root] -> [pop "GUARD:PKG"] -> [pop PKG_NAME]* -> [push PKG_INTERNAL_NAME] -> [push "GUARD:PKG_INTERNAL"] -> [root]
+        //
+        if !npm_pkg.name.is_empty() {
+            // reach package internals via package name
+            //
+            //     [root] -> [pop "GUARD:PKG"] -> [pop pkg_name]* -> [push pkg_internal_name]
+            //
+            let pkg_guard_pop = add_pop(graph, file, root, PKG_GUARD, "");
+            let pkg_name_pop =
+                add_module_pops(graph, file, Path::new(&npm_pkg.name), pkg_guard_pop, "");
+            add_edge(graph, pkg_name_pop, pkg_internal_name_push, 0);
+
+            // reach main exports directly via package name (with precedence)
+            //
+            //     [pop pkg_name] -1-> [pop "GUARD:EXPORTS"] -> [push "GUARD:EXPORTS"] -> [push main]* -> [push pkg_internal_name]
+            //
+            let exports_guard_pop = add_pop(graph, file, pkg_name_pop, EXPORTS_GUARD, "");
+            replace_edge(graph, pkg_name_pop, exports_guard_pop, 1);
+            let main = NormalizedRelativePath::from_str(&npm_pkg.main)
+                .map(|p| p.into_path_buf())
+                .unwrap_or(PathBuf::from("index"))
+                .with_extension("")
+                .with_extension("");
+            let main_push = add_module_pushes(graph, file, &main, pkg_internal_name_push, "");
+            let exports_guard_push = add_push(graph, file, main_push, EXPORTS_GUARD, "");
+            add_edge(graph, exports_guard_pop, exports_guard_push, 0);
+        }
+
+        // reach dependencies via package internal name
+        //
+        //     [pop pkg_internal_name] -> [pop "GUARD:PKG"] -> [pop dep_name]* -> [push dep_name]* -> [push "GUARD:PKG"] -> [root]
+        //
+        let dep_guard_pop = add_pop(graph, file, pkg_internal_name_pop, PKG_GUARD, "");
+        let dep_guard_push = add_push(graph, file, root, PKG_GUARD, "");
+        for (_, (dep_name, _)) in npm_pkg.dependencies.iter().enumerate() {
+            if dep_name.is_empty() {
+                continue;
+            }
+            let dep_name_pop = add_module_pops(graph, file, Path::new(dep_name), dep_guard_pop, "");
+            let dep_name_push =
+                add_module_pushes(graph, file, Path::new(dep_name), dep_guard_push, "");
+            add_edge(graph, dep_name_pop, dep_name_push, 0);
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct NpmPackage {
+    pub name: String,
+    pub main: String,
+    #[serde(default)]
+    pub dependencies: HashMap<String, serde_json::Value>,
+}
