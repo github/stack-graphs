a59505546927347f0e3c559465c7e5db9aa0d94a
Hendrik van Antwerpen <hendrikvanantwerpen@github.com>
2023-10-10 12:18:29 +0200

    Adds first vea test
    Co-authored-by: Rebecca Valentine <beka@gothdyke.mom>

---
+++ languages/tree-sitter-stack-graphs-javascript/test/old/vea_validation_definition_baselines/ES6_import_export_patterns/export_with_name_default_alias_default.js
@@ -0,0 +1,14 @@
+/*--- path: a.js ---*/
+
+export default function foo() { }
+
+/*--- path: index.js ---*/
+
+export { default as default } from "./a.js";
+
+// --- path: index2.js ---
+
+import bar from "./index.js";
+
+/**/ bar;
+//   ^ defined: 3, 7, 11
