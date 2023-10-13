80d73caab8b2b32d0443308cfa683415a7433ff0
Hendrik van Antwerpen <hendrikvanantwerpen@github.com>
2023-10-13 11:58:52 +0200

    Make subscripts proper references

--- languages/tree-sitter-stack-graphs-javascript/test/old/compound_literals/objects.js
+++ languages/tree-sitter-stack-graphs-javascript/test/old/compound_literals/objects.js
@@ -1,15 +1,24 @@
-let obj = { x: 1, y: 2, 0: "z" };
+let obj = {
+    x: 1, y: 2, 0: "z" };
+
 let obj_x = obj.x;
+//          ^ defined: 1
+//              ^ defined: 2
 let x = obj_x;
-//      ^ defined: 1, 2
+//      ^ defined: 4, 2
+
 let obj_y = obj["y"];
+//          ^ defined: 1
+//              ^ defined: 2
 let y = obj_y;
-//      ^ defined: 1, 5
+//      ^ defined: 10, 2
 
 let obj_z = obj[0];
+//          ^ defined: 1
+//              ^ defined: 2
 let z = obj_z;
-//      ^ defined: 1, 9
+//      ^ defined: 16, 2
 
 let obj2 = { x, y };
-//           ^ defined: 1, 2, 3
-//              ^ defined: 1, 5, 6
+//           ^ defined: 7, 4, 2
+//              ^ defined: 13, 10, 2
---
+++ languages/tree-sitter-stack-graphs-javascript/test/old/simple_expressions/template_strings.js
@@ -0,0 +1,4 @@
+let x = 1;
+
+let y = `template ${ x } string`;
+//                   ^ defined: 1
