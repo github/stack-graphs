a2dc6158488db02a3fe0cf4730412d3f17e8d868
Hendrik van Antwerpen <hendrikvanantwerpen@github.com>
2023-11-07 13:48:52 +0100

    Only export main module from package

---
+++ languages/tree-sitter-stack-graphs-javascript/test/old/packages/can-access-package-non-main-explicilty.js
@@ -0,0 +1,39 @@
+/* --- path: acme_foo/package.json --- */
+/* --- global: FILE_PATH=package.json --- */
+/* --- global: PROJECT_NAME=acme_foo --- */
+
+{
+    "name": "@acme/foo",
+    "version": "1.0",
+    "main": "./api"
+}
+
+/* --- path: acme_foo/api.js --- */
+/* --- global: FILE_PATH=api.js --- */
+/* --- global: PROJECT_NAME=acme_foo --- */
+
+export let x;
+
+/* --- path: acme_foo/core.js --- */
+/* --- global: FILE_PATH=core.js --- */
+/* --- global: PROJECT_NAME=acme_foo --- */
+
+export let x;
+
+/* --- path: bar/package.json --- */
+/* --- global: FILE_PATH=package.json --- */
+/* --- global: PROJECT_NAME=bar --- */
+
+{
+    "name": "bar",
+    "dependencies": {
+        "@acme/foo": "1"
+    }
+}
+
+/* --- path: bar/app.js --- */
+/* --- global: FILE_PATH=app.js --- */
+/* --- global: PROJECT_NAME=bar --- */
+
+import { x } from "@acme/foo/core"
+//       ^ defined: 21
