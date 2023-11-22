9323b4c9325f57999a36314e87bad47a00b48c0c
Hendrik van Antwerpen <hendrikvanantwerpen@github.com>
2023-11-22 11:35:29 +0100

    Eliminate nested identifier unrolling

---
+++ languages/tree-sitter-stack-graphs-typescript/test/modules/very-deep-namespace.ts
@@ -0,0 +1,6 @@
+namespace a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p.q.r.s.t.u.v.w.x.y {
+    export const Z = 42;
+}
+
+a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p.q.r.s.t.u.v.w.x.y.Z
+//                                                ^ defined: 2
