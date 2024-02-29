88bc1700ad54d09bf30f4c3aa1e7eb2b399c7f75
Eugene Yakubovich <eyakubovich@gmail.com>
2024-02-28 17:18:26 -0800

    Support for TSX
    Support TSX dialect of the TypeScript.
    Because the TSX is almost a superset of TypeScript,
    it makes sense to reuse the .tsg file. However it
    requires a bit of preprocessing to handle the differences.

--- languages/tree-sitter-stack-graphs-typescript/rust/lib.rs
+++ languages/tree-sitter-stack-graphs-typescript/rust/lib.rs
@@ -5,6 +5,11 @@
 // Please see the LICENSE-APACHE or LICENSE-MIT files in this distribution for license details.
 // ------------------------------------------------------------------------------------------------
 
+use std::path::PathBuf;
+use std::borrow::Cow;
+
+use askama::Template;
+use lazy_static::lazy_static;
 use tree_sitter_stack_graphs::loader::LanguageConfiguration;
 use tree_sitter_stack_graphs::loader::LoadError;
 use tree_sitter_stack_graphs::CancellationFlag;
@@ -33,8 +38,23 @@ pub const FILE_PATH_VAR: &str = "FILE_PATH";
 /// The name of the project name global variable
 pub const PROJECT_NAME_VAR: &str = "PROJECT_NAME";
 
+lazy_static! {
+    static ref STACK_GRAPHS_TS_TSG_SOURCE: String = preprocess_tsg("typescript").unwrap();
+    static ref STACK_GRAPHS_TSX_TSG_SOURCE: String = preprocess_tsg("tsx").unwrap();
+}
+
+#[derive(Template)]
+#[template(path = "stack-graphs.tsg")]
+struct TsgTemplate<'a> {
+    dialect: &'a str,
+}
+
 pub fn language_configuration(cancellation_flag: &dyn CancellationFlag) -> LanguageConfiguration {
     try_language_configuration(cancellation_flag).unwrap_or_else(|err| panic!("{}", err))
+}
+
+pub fn tsx_language_configuration(cancellation_flag: &dyn CancellationFlag) -> LanguageConfiguration {
+    try_tsx_language_configuration(cancellation_flag).unwrap_or_else(|err| panic!("{}", err))
 }
 
 pub fn try_language_configuration(
@@ -46,7 +66,7 @@ pub fn try_language_configuration(
         None,
         vec![String::from("ts")],
         STACK_GRAPHS_TSG_PATH.into(),
-        STACK_GRAPHS_TSG_SOURCE,
+        &STACK_GRAPHS_TS_TSG_SOURCE,
         Some((
             STACK_GRAPHS_BUILTINS_PATH.into(),
             STACK_GRAPHS_BUILTINS_SOURCE,
@@ -59,4 +79,38 @@ pub fn try_language_configuration(
         .add("package.json".to_string(), NpmPackageAnalyzer {});
     lc.no_similar_paths_in_file = true;
     Ok(lc)
+}
+
+pub fn try_tsx_language_configuration(
+    cancellation_flag: &dyn CancellationFlag,
+) -> Result<LanguageConfiguration, LoadError> {
+    let mut lc = LanguageConfiguration::from_sources(
+        tree_sitter_typescript::language_tsx(),
+        Some(String::from("source.tsx")),
+        None,
+        vec![String::from("tsx")],
+        STACK_GRAPHS_TSG_PATH.into(),
+        &STACK_GRAPHS_TSX_TSG_SOURCE,
+        Some((
+            STACK_GRAPHS_BUILTINS_PATH.into(),
+            STACK_GRAPHS_BUILTINS_SOURCE,
+        )),
+        Some(STACK_GRAPHS_BUILTINS_CONFIG),
+        cancellation_flag,
+    )?;
+    lc.special_files
+        .add("tsconfig.json".to_string(), TsConfigAnalyzer {})
+        .add("package.json".to_string(), NpmPackageAnalyzer {});
+    lc.no_similar_paths_in_file = true;
+    Ok(lc)
+}
+
+fn preprocess_tsg(dialect: &'static str) -> Result<String, LoadError<'static>> {
+    let tmpl = TsgTemplate{ dialect };
+    tmpl.render()
+        .map_err(|err| LoadError::PreprocessorError {
+            inner: err.to_string(),
+            tsg_path: PathBuf::from(STACK_GRAPHS_TSG_PATH),
+            tsg: Cow::from(STACK_GRAPHS_TSG_SOURCE),
+        })
 }
