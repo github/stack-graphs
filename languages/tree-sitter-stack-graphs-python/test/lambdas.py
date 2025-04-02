5d3b5a6b7a543546b32c3bf96215c65fc41dfd17
Bastiaan Marinus van de Weerd <ext-b.vandeweerd@globant.com>
2025-04-01 21:20:17 -0300

    Fix lambdas without params. in Python.

--- languages/tree-sitter-stack-graphs-python/test/lambdas.py
+++ languages/tree-sitter-stack-graphs-python/test/lambdas.py
@@ -1,2 +1,11 @@
 sorted([1, 2, 3], key=lambda x: x)
 #                               ^ defined: 1
+
+y = 42
+
+foo = lambda: print(y)
+#                   ^ defined: 4
+
+bar = lambda daz: print(y, daz)
+#                       ^ defined: 4
+#                          ^ defined: 9
