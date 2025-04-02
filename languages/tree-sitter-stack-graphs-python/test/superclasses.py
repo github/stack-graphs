d6047439ae4e9efc418269817c7c8047d2794b5a
Bastiaan Marinus van de Weerd <ext-b.vandeweerd@globant.com>
2025-04-01 21:52:53 -0300

    Fix supercclass keyword arguments in Python.

--- languages/tree-sitter-stack-graphs-python/test/superclasses.py
+++ languages/tree-sitter-stack-graphs-python/test/superclasses.py
@@ -1,19 +1,22 @@
 class A:
+    def __init_subclass__(cls, foo):
+        pass
+
     def __init__(self):
         self.some_attr = 2
 
     def some_method(self):
         print self
 
-class B(A):
+class B(A, foo="Bar"):
     def method2(self):
         print self.some_attr, self.some_method()
-        #          ^ defined: 3
-        #                          ^ defined: 5, 14
+        #          ^ defined: 6
+        #                          ^ defined: 8, 17
 
     def some_method(self):
         pass
 
     def other(self):
         super().some_method()
-        #       ^ defined: 5
+        #       ^ defined: 8
