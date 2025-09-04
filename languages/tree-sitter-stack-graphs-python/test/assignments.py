c6390ae4da5b49cb0e2a787f2892b74519de759c
Lexxxzy <lex.halikov@yandex.ru>
2025-09-04 20:04:09 +0300

    feat(python):  Chained assignments support

---
+++ languages/tree-sitter-stack-graphs-python/test/assignments.py
@@ -0,0 +1,12 @@
+exponent = exp = 3
+digits = decimals = abs(exp)
+                    #   ^ defined: 1
+
+print digits
+#     ^ defined: 2
+
+print decimals
+#     ^ defined: 2
+
+print exponent
+#     ^ defined: 1
