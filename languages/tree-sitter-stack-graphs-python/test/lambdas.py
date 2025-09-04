e30db7fbafe442804ec5e0c0fda2f9f37f1aa2e3
Lexxxzy <lex.halikov@yandex.ru>
2025-09-04 20:04:09 +0300

    fix(python): Improve lambda function handling

--- languages/tree-sitter-stack-graphs-python/test/lambdas.py
+++ languages/tree-sitter-stack-graphs-python/test/lambdas.py
@@ -1,2 +1,11 @@
-sorted([1, 2, 3], key=lambda x: x)
-#                               ^ defined: 1
+y = x = 1
+
+no_params = lambda: y
+            #       ^ defined: 1
+
+sorted([1, 2, 3], key=lambda y: x + y)
+                      #         ^ defined: 1
+
+def uses_default(fn=lambda: 1):
+    fn()
+#   ^ defined: 9
